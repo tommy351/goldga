b772d79ae4b62d2379c4b1682ed0b6df5ac3cc76
Tommy Chen <tommy351@gmail.com>
2020-02-02 00:35:38 +0800

    test: Generate junit reports

--- golden/main_test.go
+++ golden/main_test.go
@@ -1,13 +1,29 @@
 package golden
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
 )
 
 func Test(t *testing.T) {
+	var specReporters []Reporter
+
+	if dir := os.Getenv("JUNIT_OUTPUT"); dir != "" {
+		if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+			panic(err)
+		}
+
+		path := filepath.Join(dir, fmt.Sprintf("junit-%d.xml", time.Now().UnixNano()))
+		specReporters = append(specReporters, reporters.NewJUnitReporter(path))
+	}
+
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "golden")
+	RunSpecsWithDefaultAndCustomReporters(t, "golden", specReporters)
 }
