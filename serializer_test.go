ccc33f0e63a3a20226b71e55d3ab20b07fa6a593
Tommy Chen <tommy351@gmail.com>
2020-02-02 01:59:48 +0800

    test: Add tests of serializers

---
+++ serializer_test.go
@@ -0,0 +1,54 @@
+package goldga
+
+import (
+	"bytes"
+	"encoding/json"
+
+	"github.com/BurntSushi/toml"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"gopkg.in/yaml.v2"
+)
+
+// nolint: gochecknoglobals
+var serializerTestData = map[string]interface{}{
+	"a": "str",
+	"b": true,
+	"c": 3.14,
+}
+
+func testSerializer(s Serializer) []byte {
+	var buf bytes.Buffer
+	Expect(s.Serialize(&buf, serializerTestData)).To(Succeed())
+	return buf.Bytes()
+}
+
+var _ = Describe("YAMLSerializer", func() {
+	It("works", func() {
+		expected, err := yaml.Marshal(serializerTestData)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(testSerializer(&YAMLSerializer{})).To(MatchYAML(expected))
+	})
+})
+
+var _ = Describe("JSONSerializer", func() {
+	It("works", func() {
+		expected, err := json.Marshal(serializerTestData)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(testSerializer(&JSONSerializer{})).To(MatchJSON(expected))
+	})
+})
+
+var _ = Describe("TOMLSerializer", func() {
+	marshalTOML := func(input interface{}) []byte {
+		var buf bytes.Buffer
+		enc := toml.NewEncoder(&buf)
+		Expect(enc.Encode(input)).To(Succeed())
+		return buf.Bytes()
+	}
+
+	It("works", func() {
+		expected := marshalTOML(serializerTestData)
+		Expect(testSerializer(&TOMLSerializer{})).To(Equal(expected))
+	})
+})
